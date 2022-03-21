24d5d2d6e6002e334abe675cee36f371b2c8c03d
Long Pham <philong.bmt@gmail.com>
2022-03-21 19:37:54 +0700

    log readCardAccess command

--- Sources/NFCPassportReader/TagReader.swift
+++ Sources/NFCPassportReader/TagReader.swift
@@ -232,7 +232,8 @@ public class TagReader {
         // By executing above SELECT command (with data=0x3F00) master file should be selected and you should be able to read EF.CardAccess from passport.
         
         // First select master file
-        let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x00, p2Parameter: 0x0C, data: Data([0x3f,0x00]), expectedResponseLength: 256)
+        let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x00, p2Parameter: 0x0C, data: Data([0x3f,0x00]), expectedResponseLength: -1)
+        Log.verbose( "readCardAccess = \(cmd)" )
         
         send( cmd: cmd) { response, error in
             if let error = error {
